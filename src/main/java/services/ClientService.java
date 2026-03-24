e1f77a6abbeeb13bffa4d3c323dc0e49905c316b
BohdanKalitovskyi <kalitbo4132_aehit@students.vizja.pl>
2026-03-24 16:04:20 +0100

    TransactionHelper + ClientService

---
+++ src/main/java/services/ClientService.java
@@ -0,0 +1,51 @@
+package services;
+
+import hib.proj.Client;
+import hib.proj.TransactionHelper;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class ClientService {
+    private final SessionFactory sessionFactory;
+    private final TransactionHelper transactionHelper;
+
+    public ClientService(SessionFactory sessionFactory, TransactionHelper transactionHelper) {
+        this.sessionFactory = sessionFactory;
+        this.transactionHelper = transactionHelper;
+    }
+
+    public Client saveClient(Client client){
+        return transactionHelper.executeInTransaction(session -> {
+           session.persist(client);
+           return client;
+        });
+    }
+
+    public void deleteClient(long id){
+        transactionHelper.executeInTransaction(session -> {
+            Client clientToDelete = session.get(Client.class,id);
+            session.remove(clientToDelete);
+        });
+    }
+
+    public Client getById(long id){
+        try(Session session = sessionFactory.openSession()){
+            return  session.get(Client.class, id);
+        }
+    }
+
+    public List<Client> findAll(){
+        try(Session session = sessionFactory.openSession()){
+            return session.createQuery("SELECT s from Client s ",Client.class).list();
+        }
+    }
+
+    public Client updateClient(Client client){
+        return transactionHelper.executeInTransaction(session -> {
+           return session.merge(client);
+        });
+    }
+
+}
